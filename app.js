4eca4ee150568d52f00f1c3befc15d7dacf3c89e
femithz <ajayiadeyinka6991@gmail.com>
2021-03-29 21:30:36 +0100

    update

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const  express = require('express');
-const path =  require('path');
-const bodyparser = require('body-parser');
-const port = process.env.PORT || 8080
+const express=require('express');
+const path=require('path');
+const bodyparser=require('body-parser');
+const cors = require('cors');
+const http = require('https');
+
+
+
 
 var app = express();
+
+const port=process.env.PORT || 8080;
+app.use(cors());
+
+app.use(cors({
+    origin:'*'
+  }));
 // view engine setup
-app.use(express.static(path.join(__dirname, 'public/index.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyparser.json());
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
 
-app.listen(port, process.env.IP, () =>{
-     console.log(port);
+app.get('*', (req,res) => {
+   res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(port, process.env.IP, () => {
+         console.log('Server is running on' + port);        
 })
