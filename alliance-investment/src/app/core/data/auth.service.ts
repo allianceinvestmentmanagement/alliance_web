d374177263e71f3d4b799827ff03d31918d11003
femithz <ajayiadeyinka6991@gmail.com>
2021-05-09 12:46:44 +0100

    update

--- alliance-investment/src/app/core/data/auth.service.ts
+++ alliance-investment/src/app/core/data/auth.service.ts
@@ -1,12 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-const url = "https://allianceapi.herokuapp.com/api/v1";
-// ";
-//  "https://allianceapi.herokuapp.com/api/v1";
-// "http://localhost:3000/api/v1";
-// "https://allianceapi.herokuapp.com/api/v1";  
-// 
-//  
+const url = "https://allianceapi.herokuapp.com/api/v1";  
+
 @Injectable({
   providedIn: 'root'
 })
